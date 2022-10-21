6c85e5d06ee1653d7e39a3bbd5f7c9c35d504961
DipodDP <dipoddp@gmail.com>
2022-10-21 12:21:28 +0700

    Update wg_gencli.sh

--- clients/wg_gencli.sh
+++ clients/wg_gencli.sh
@@ -6,7 +6,8 @@ CN="${CURRENT_IP##*.}"
 NEW_IP=10.10.0.$(($CN + 1))
 
 mkdir $1
-wg genkey | tee ./$1/client.key |  wg pubkey | tee ./$1/client.key.pub | awk '/^/{>cp client.conf ./$1/client.conf
+wg genkey | tee ./$1/client.key |  wg pubkey | tee ./$1/client.key.pub | awk '/^/{print "Public Key: "$1}'
+cp client.conf ./$1/client.conf
 sed -i 's~privatekeytoreplace~'$(cat ./$1/client.key)'~g' ./$1/client.conf
 sed -i 's~'"$IP_RANGE"'~'"$NEW_IP"'~g' ./$1/client.conf
 echo "\n *** Client config: ***\n"
@@ -16,6 +17,7 @@ qrencode -t ansiutf8 < ./$1/client.conf
 echo $NEW_IP > state
 
 echo "*** Next lines has been added to wg0.conf: ***"
-echo "\n[Peer]\nPublicKey = $(cat ./$1/client.key.pub)\nAllowedIPs = ${NEW_IP}/32\>
+echo "\n[Peer]\nPublicKey = $(cat ./$1/client.key.pub)\nAllowedIPs = ${NEW_IP}/32\n" | tee -a $WG_CONF_PATH
+
 systemctl restart wg-quick@wg0
 wg show
