5b56a6fab332f67701d280394ed393d2c12ef165
root <root@srv27500.blue.kundencontroller.de>
2023-04-17 04:35:14 +0000

    feat: added ipv6 support for dualstack VPSes

--- clients/wg_gencli.sh
+++ clients/wg_gencli.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
-IP_RANGE=10.10.0.0
+IP6_RANGE=$(cat ipv6subnet)
 WG_CONF_PATH="/etc/wireguard/wg0.conf"
-CURRENT_IP=$(cat state)
+WG_KEY_PATH="/etc/wireguard/publickey"
+CURRENT_IP=$(cat ipv4state)
 CN="${CURRENT_IP##*.}"
-NEW_IP=10.10.0.$(($CN + 1))
-
+NEW_IP="${CURRENT_IP%.*}.$(($CN + 1))"
 mkdir $1
 wg genkey | tee ./$1/client.key |  wg pubkey | tee ./$1/client.key.pub | awk '/^/{print "Public Key: "$1}'
 cp client.conf ./$1/client.conf
-sed -i 's~privatekeytoreplace~'$(cat ./$1/client.key)'~g' ./$1/client.conf
-sed -i 's~'"$IP_RANGE"'~'"$NEW_IP"'~g' ./$1/client.conf
+sed -i 's~private_key_to_replace~'$(cat ./$1/client.key)'~g' ./$1/client.conf
+sed -i 's~server_public_key_to_replace~'$(cat $WG_KEY_PATH)'~g' ./$1/client.conf
+sed -i 's~client_addresses_to_replace~'"$NEW_IP/32, $IP6_RANGE$(($CN + 1))/128"'~g' ./$1/client.conf
 echo "\n *** Client config: ***\n"
 cat ./$1/client.conf
 qrencode -o ./$1/client.png < ./$1/client.conf
 qrencode -t ansiutf8 < ./$1/client.conf
-echo $NEW_IP > state
+echo $NEW_IP > ipv4state
 
 systemctl stop wg-quick@wg0
 
 echo "*** Next lines has been added to wg0.conf: ***"
-echo "\n[Peer]\nPublicKey = $(cat ./$1/client.key.pub)\nAllowedIPs = ${NEW_IP}/32\n" | tee -a $WG_CONF_PATH
+echo "\n[Peer]\nPublicKey = $(cat ./$1/client.key.pub)\nAllowedIPs = ${NEW_IP}/32, $IP6_RANGE$(($CN + 1))/128\n" | tee -a $WG_CONF_PATH
 
 systemctl start wg-quick@wg0
 wg show
